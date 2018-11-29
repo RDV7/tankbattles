61dba14d0d220bceb13e24bbfbcd8f3d42f379cb
ruben <rubendevilliers01@gmail.com>
2018-11-29 13:29:36 +0200

    init project

---
+++ tankbattles/Source/tankbattles/tankbattlesGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "tankbattlesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKBATTLES_API AtankbattlesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
